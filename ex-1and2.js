3a210553aefa387a60d484dec9c65c5adcf79c63
pang <pangton28.p@gmail.com>
2023-11-23 13:25:18 +0700

    feat(exercise): values-variables-js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurant = "Chef's Table by Chef James";
+const foodPriceBill = 15000;
+const restaurantsCurrentlyOpen = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292500 County Rd #4Meeker, Colorado(CO), 81641"
+foodPriceBill = 5000;
+restaurantsCurrentlyOpen = 2500;
