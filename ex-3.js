17003bdf5362f222e922a1970ee07dbe5bdae5a1
Ping <ping15512@gmail.com>
2023-11-23 15:57:37 +0700

    add:add answer by ping

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
+
+// Start coding here
 let celsius = 30;
+let fahrenheit = celsius * 1.8 + 32;
 
-
-
-let fahrenheit = (celsius * 1.8) + 32;
+console.log(fahrenheit);
