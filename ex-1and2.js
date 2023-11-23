ed601177613207e56b086be62aa1375046e7f2eb
du <tb.poopp@gmail.com>
2023-11-23 13:36:38 +0700

    feat: add exercise values variable

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let personName = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName =  "Chef's Table by Chef James";
+const foodPrice = 15000;
+const totalRestaurantCurrentlyOpen = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”;
+foodPrice = 5000;
+totalRestaurantCurrentlyOpen = 2500;
